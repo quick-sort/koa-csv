d819dbcbc1c41ce2fbae8f71d33f59892515e493
Rui Zhou <quicksort@outlook.com>
2018-05-14 23:45:24 +0800

    add feature 'lsdir'

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # koa-csv
+[![NPM version][npm-image]][npm-url]
+[![Build status][travis-image]][travis-url]
+[![Test coverage][coveralls-image]][coveralls-url]
+[![License][license-image]][license-url]
+[![Downloads][downloads-image]][downloads-url]
 CSV files serving as json API middleware for koa
 
 ## Usage Example
@@ -14,12 +19,13 @@ app.use(require('koa-csv')(root, opts))
 
 ## Request Example 
 for `test.csv` like this
+
 Name|Value
 ----|-----
 name|value
 
 ```
-GET /test
+GET /test.csv
 ```
 ```json
 [{
@@ -29,7 +35,7 @@ GET /test
 ```
 
 ```
-GET /test?axis=col
+GET /test.csv?axis=col
 ```
 ```json
 {
@@ -45,7 +51,20 @@ GET /test?axis=col
 - `axis` default is `row`, csv will be converted to an array of objects representing each line in csv, if `axis` is `col`, csv will be converted to an object, each property representing column in csv
 - `delimiter` csv delimiter, default `,`
 - `prefix` set the prefix of request path to match
+- `lsdir` enable lsdir if path is a dir, default false
 
 ## TODO
 - gzip support
 
+[npm-image]: https://img.shields.io/npm/v/koa-csv.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/koa-csv
+[github-tag]: http://img.shields.io/github/tag/koajs/csv.svg?style=flat-square
+[github-url]: https://github.com/quick-sort/koa-csv/tags
+[travis-image]: https://img.shields.io/travis/quick-sort/koa-csv.svg?style=flat-square
+[travis-url]: https://travis-ci.org/quick-sort/koa-csv
+[coveralls-image]: https://img.shields.io/coveralls/quick-sort/koa-csv.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/quick-sort/koa-csv?branch=master
+[license-image]: http://img.shields.io/npm/l/koa-csv.svg?style=flat-square
+[license-url]: LICENSE
+[downloads-image]: http://img.shields.io/npm/dm/koa-csv.svg?style=flat-square
+[downloads-url]: https://npmjs.org/package/koa-csv
--- index.js
+++ index.js
@@ -2,34 +2,55 @@
 const debug = require('debug')('koa-csv')
 const csv2json = require('./csv2json')
 const path = require('path')
+const fs = require('fs')
+const util = require('util')
 const createError = require('http-errors')
 module.exports = serve
+const readdir = util.promisify(fs.readdir)
 function serve (root, options) {
-    let opts = Object.assign({}, options)
-    let rootAbs = path.resolve(process.cwd(), root)
-    debug('serving csv folder %s', rootAbs)
-    return async function serve (ctx, next) {
-        let done = false
-        let prefix = opts.prefix || '/'
-        if (prefix[0] !== '/') {
-            prefix = '/' + prefix
-        }
-        if (ctx.method === 'HEAD' || ctx.method === 'GET') {
-            if (prefix === ctx.path.substr(0, prefix.length)) {
-                let ipath = ctx.path.substr(prefix.length)
-                let file = path.join(rootAbs, ipath + '.csv')
-                if (rootAbs === file.substr(0, rootAbs.length)) {
-                    debug('serving %s.csv', ipath)
-                    ctx.body = await csv2json(file, Object.assign({}, options, ctx.query))
-                } else {
-                    debug('cannot find %s.csv', ipath)
-                    throw new createError.NotFound()
-                }
-                done = true
-            }
-        }
-        if (!done) {
-            await next()
-        }
+  let opts = Object.assign({}, options)
+  let rootAbs = path.resolve(process.cwd(), root)
+  debug('serving csv folder %s', rootAbs)
+  return async function serve (ctx, next) {
+    let done = false
+    let prefix = opts.prefix || '/'
+    if (prefix[0] !== '/') {
+      prefix = '/' + prefix
     }
+    if (prefix[prefix.length - 1] !== '/') {
+      prefix = prefix + '/'
+    }
+    if (ctx.method === 'HEAD' || ctx.method === 'GET') {
+      if (prefix === ctx.path.substr(0, prefix.length)) {
+        let ipath = ctx.path.substr(prefix.length)
+        if (ipath) {
+          if (ipath.endsWith('.csv')) {
+            let file = path.join(rootAbs, ipath)
+            if (rootAbs === file.substr(0, rootAbs.length)) {
+              debug('serving %s.csv', ipath)
+              ctx.body = await csv2json(file, Object.assign({}, options, ctx.query))
+            } else {
+              debug('cannot find %s.csv', ipath)
+              throw new createError.NotFound()
+            }
+          } else if (options.lsdir === true) {
+            let dir = path.join(rootAbs, ipath)
+            if (rootAbs === dir.substr(0, rootAbs.length)) {
+              debug('ls %s', ipath)
+              ctx.body = await readdir(dir)
+            } else {
+              debug('cannot dir %s', ipath)
+              throw new createError.NotFound()
+            }
+          } else {
+              throw new createError.NotFound()
+          }
+        }
+        done = true
+      }
+    }
+    if (!done) {
+      await next()
+    }
+  }
 }
